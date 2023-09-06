83b3ee7a99d2c3463b620f7bc80c5920d7fe9025
devSeyeong <tpdud3131@gmail.com>
2023-09-06 13:46:16 +0900

    Change stateless widget to stateful widget

---
+++ lib/stateless_to_stateful_widget_demo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title:'Stateless -> Stateful Widget Demo',
+  home: Scaffold(
+  appBar: AppBar(title: Text('Stateless -> Stateful 위젯 데모')),
+  body: _FirstStatefulWidget(),
+),
+));
+
+class _FirstStatefulWidget extends StatefulWidget {
+  @override
+  State createState() => _FirstStatefulWidgetState();
+  
+}
+
+class _FirstStatefulWidgetState extends State<_FirstStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('이것은 stateful 위젯입니다.');
+  }
+}
