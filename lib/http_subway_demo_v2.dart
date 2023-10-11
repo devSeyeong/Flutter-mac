c72a9f3d1f6d3ef2a04744ccefcad1c77521c7e1
devSeyeong <tpdud3131@gmail.com>
2023-10-11 20:02:59 +0900

    [Add] Subway api subway_demo_v2

---
+++ lib/http_subway_demo_v2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'page_subway_main.dart';
+
+void main() => runApp(const SubwayDemo());
+
+class SubwayDemo extends StatelessWidget {
+  const SubwayDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: '지하철 실시간 정보',
+      home: MainPage(),
+    );
+  }
+}
